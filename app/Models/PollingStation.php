891829a67c158f6bdc64ee24a1a68ab344bbd378
stekl0 <greystekl0@gmail.com>
2025-10-07 20:56:35 +0500

    Add relationships between PollingStation and Region models

--- app/Models/PollingStation.php
+++ app/Models/PollingStation.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PollingStation extends Model
 {
-    //
+    /**
+     * @return BelongsTo<Region, $this>
+     */
+    public function region(): BelongsTo
+    {
+        return $this->belongsTo(Region::class);
+    }
 }
--- app/Models/Region.php
+++ app/Models/Region.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Region extends Model
 {
-    //
+    /**
+     * @return HasMany<PollingStation, $this>
+     */
+    public function pollingStations(): HasMany
+    {
+        return $this->hasMany(PollingStation::class);
+    }
 }
