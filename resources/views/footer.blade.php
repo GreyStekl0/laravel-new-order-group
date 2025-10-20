a6e772e847d93393c9c8d524edf6e6fb5d3b336c
stekl0 <greystekl0@gmail.com>
2025-10-20 23:56:03 +0500

    add baze

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="mt-auto py-3 bg-body-tertiary border-top">
+    <div class="container text-center text-body-secondary">
+        <p class="mb-0 small">
+            &copy; {{ now()->year }} {{ config('app.name', 'ООО «Честные выборы для честных кандидатов»') }}
+        </p>
+    </div>
+</footer>
