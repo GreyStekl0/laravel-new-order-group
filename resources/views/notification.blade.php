2081a6602753b28bfcb49e2ba52a4e2f269dfe77
stekl0 <greystekl0@gmail.com>
2025-10-21 20:11:44 +0500

    refac

--- resources/views/notification.blade.php
+++ resources/views/notification.blade.php
@@ -1,15 +1,11 @@
 @if ($msg = session('success'))
-    <div class="toast-container position-fixed top-0 start-50 translate-middle-x p-3"
-         style="z-index:1100" aria-live="polite" aria-atomic="true">
-        <div class="toast align-items-center text-bg-success border-0"
-             role="status" aria-live="polite" aria-atomic="true"
-             data-bs-delay="4000" data-auto-show="true">
+    <div class="toast-container position-fixed top-0 start-50 translate-middle-x p-3" style="z-index:1100">
+        <div class="toast align-items-center text-bg-success border-0" data-bs-delay="4000" data-auto-show="true">
             <div class="d-flex">
                 <div class="toast-body">
-                    <i class="fa-solid fa-circle-check me-2" aria-hidden="true"></i>{{ $msg }}
+                    <i class="fa-solid fa-circle-check me-2"></i>{{ $msg }}
                 </div>
-                <button type="button" class="btn-close btn-close-white me-2 m-auto"
-                        data-bs-dismiss="toast" aria-label="Закрыть"></button>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"></button>
             </div>
         </div>
     </div>
