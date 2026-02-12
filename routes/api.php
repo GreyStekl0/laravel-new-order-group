12efc1869cd054f673a26bc12c71a4a686b7fb81
Maxim Dubrovsky <maxdubmors@gmail.com>
2026-02-12 19:07:25 +0500

    Установка Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CandidateControllerApi;
 use App\Http\Controllers\PollingStationControllerApi;
 use App\Http\Controllers\RegionControllerApi;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,11 +17,20 @@ use App\Http\Controllers\RegionControllerApi;
 |
 */
 
-Route::get('/PollingStation', [PollingStationControllerApi::class, 'index']);
-Route::get('/PollingStation/{pollingStation}', [PollingStationControllerApi::class, 'show']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/Candidate', [CandidateControllerApi::class, 'index']);
-Route::get('/Candidate/{candidate}', [CandidateControllerApi::class, 'show']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/logout', [AuthController::class, 'logout']);
 
-Route::get('/Region', [RegionControllerApi::class, 'index']);
-Route::get('/Region/{region}', [RegionControllerApi::class, 'show']);
+    Route::get('/PollingStation', [PollingStationControllerApi::class, 'index']);
+    Route::get('/PollingStation/{pollingStation}', [PollingStationControllerApi::class, 'show']);
+
+    Route::get('/Candidate', [CandidateControllerApi::class, 'index']);
+    Route::get('/Candidate/{candidate}', [CandidateControllerApi::class, 'show']);
+
+    Route::get('/Region', [RegionControllerApi::class, 'index']);
+    Route::get('/Region/{region}', [RegionControllerApi::class, 'show']);
+});
